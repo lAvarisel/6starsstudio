b005e1d07df70222ac769ee7ab6a41228c6f74a3
Dmitry Melnikov <dmitry.melni@gmail.com>
2020-06-11 00:10:27 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch, 
+  Route
+} from 'react-router-dom';
+import Home from './Pages/Home/Home';
+import About from './Pages/About/About';
+import Clients from './Pages/Clients/Clients';
+import Contact from './Pages/Contact/Contact';
+import Service from './Pages/Service/Service';
+import Work from './Pages/Work/Work';
+// import './App.css';
+
+const Main = () => (
+  <main>
+    <Switch>
+      <Route path='/About'>
+        <About/>
+      </Route>
+      <Route path='/Clients'>
+        <Clients/>
+      </Route>
+      <Route path='/Contact'>
+        <Contact/>
+      </Route>
+      <Route path='/Service'>
+        <Service/>
+      </Route>
+      <Route path='/Work'>
+        <Work/>
+      </Route>
+      <Route path='/'>
+        <Home/>
+      </Route>
+    </Switch>
+  </main>
+)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      {/* <Home/> */}
+      <Main/>
+    </Router>
   );
 }
 
