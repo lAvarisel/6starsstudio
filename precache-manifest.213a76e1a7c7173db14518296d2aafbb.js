ee51b23b82849102d4501e2ad9f5e0c8b4aa2627
Dmitry Melnikov <dmitry.melni@gmail.com>
2020-10-29 11:27:12 +0300

    Updates

---
+++ precache-manifest.213a76e1a7c7173db14518296d2aafbb.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b79ea399a69e200fea275e1f38672fb",
+    "url": "/6starsstudio/index.html"
+  },
+  {
+    "revision": "f609d461cec87f88cede",
+    "url": "/6starsstudio/static/css/main.82a3e9c8.chunk.css"
+  },
+  {
+    "revision": "c17433cc3d857ef56275",
+    "url": "/6starsstudio/static/js/2.eaa95108.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/6starsstudio/static/js/2.eaa95108.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f609d461cec87f88cede",
+    "url": "/6starsstudio/static/js/main.603d644f.chunk.js"
+  },
+  {
+    "revision": "dfcd312e79902394d51b",
+    "url": "/6starsstudio/static/js/runtime-main.f97283ad.js"
+  },
+  {
+    "revision": "c62aded729bf7146d491275e5019d7fc",
+    "url": "/6starsstudio/static/media/Gilroy-Light.c62aded7.otf"
+  },
+  {
+    "revision": "4ec0aec05eda0106b1d3c8dce89540f7",
+    "url": "/6starsstudio/static/media/advert.4ec0aec0.svg"
+  },
+  {
+    "revision": "6b5e8ef3b88310ef7327a80cb06ae7f0",
+    "url": "/6starsstudio/static/media/audit.6b5e8ef3.svg"
+  },
+  {
+    "revision": "0e2b02f1c4cfe7e7ad0605904b6046a9",
+    "url": "/6starsstudio/static/media/bgvideo.0e2b02f1.mp4"
+  },
+  {
+    "revision": "5a90dd5068d38d1efaeba87907b56e13",
+    "url": "/6starsstudio/static/media/branding.5a90dd50.svg"
+  },
+  {
+    "revision": "28a6c3ed6ab354909111ed96087237f2",
+    "url": "/6starsstudio/static/media/consultancy.28a6c3ed.svg"
+  },
+  {
+    "revision": "d743d193c0bae2511a449d23b1e7aeb8",
+    "url": "/6starsstudio/static/media/dots.d743d193.svg"
+  },
+  {
+    "revision": "83f82964864ece300fe7e122ea2b4226",
+    "url": "/6starsstudio/static/media/ecom.83f82964.svg"
+  },
+  {
+    "revision": "c53b653c585b28732b1734a90bdf2d66",
+    "url": "/6starsstudio/static/media/seo.c53b653c.svg"
+  },
+  {
+    "revision": "7003a6ef9b590f81796bba64e3e6b3c9",
+    "url": "/6starsstudio/static/media/uxui.7003a6ef.svg"
+  },
+  {
+    "revision": "d9716325a941eca8f48edfcd3d9ec9a3",
+    "url": "/6starsstudio/static/media/webIcon.d9716325.svg"
+  }
+]);
